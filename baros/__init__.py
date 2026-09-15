6ba7160101dfaca9ec803e7422248f4fe551adda
Bdavis1390 <Brandonraydavis953242@gmail.com>
2026-09-15 17:35:12 -0400

    Export BAROS DVH research metrics

--- baros/__init__.py
+++ baros/__init__.py
@@ -1,6 +1,7 @@
 """BAROS research reference package. Non-clinical; not for patient care."""
 
 from .dose import dose_from_influence, hard_max_constraints
+from .dvh import DVHSummary, cumulative_dvh, dose_to_hottest_percent, summarize_dose, volume_at_least
 from .models import logistic_ntcp, lq_survival, poisson_tcp, weighted_mean
 from .pipeline import SCHEMA_VERSION, SyntheticCase, default_synthetic_case, run_synthetic_pipeline
 from .reference_optimizer import OptimizationResult, optimize_synthetic, tumor_survival_objective
@@ -8,6 +9,11 @@ from .reference_optimizer import OptimizationResult, optimize_synthetic, tumor_s
 __all__ = [
     "dose_from_influence",
     "hard_max_constraints",
+    "DVHSummary",
+    "cumulative_dvh",
+    "dose_to_hottest_percent",
+    "summarize_dose",
+    "volume_at_least",
     "logistic_ntcp",
     "lq_survival",
     "poisson_tcp",
