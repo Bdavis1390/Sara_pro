e819f4273b0fa37e17a3a12f09afd8d08b0d43c3
Bdavis1390 <Brandonraydavis953242@gmail.com>
2026-09-15 17:25:36 -0400

    Export BAROS synthetic pipeline API

--- baros/__init__.py
+++ baros/__init__.py
@@ -2,6 +2,7 @@
 
 from .dose import dose_from_influence, hard_max_constraints
 from .models import logistic_ntcp, lq_survival, poisson_tcp, weighted_mean
+from .pipeline import SCHEMA_VERSION, SyntheticCase, default_synthetic_case, run_synthetic_pipeline
 from .reference_optimizer import OptimizationResult, optimize_synthetic, tumor_survival_objective
 
 __all__ = [
@@ -11,6 +12,10 @@ __all__ = [
     "lq_survival",
     "poisson_tcp",
     "weighted_mean",
+    "SCHEMA_VERSION",
+    "SyntheticCase",
+    "default_synthetic_case",
+    "run_synthetic_pipeline",
     "OptimizationResult",
     "optimize_synthetic",
     "tumor_survival_objective",
